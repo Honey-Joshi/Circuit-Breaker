64ea803f807c1cc07dfe5ddf98d898356dacc100
honey-joshi <honey.joshi@paytm.com>
2023-10-17 13:28:59 +0530

    logics 1.8

--- circuit.breaker/src/main/java/com/cloud/circuit/breaker/controller/GenericRestController.java
+++ circuit.breaker/src/main/java/com/cloud/circuit/breaker/controller/GenericRestController.java
@@ -3,8 +3,6 @@ package com.cloud.circuit.breaker.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.netty.handler.codec.http.HttpResponseStatus;
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -12,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.RequestEntity;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,9 +20,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
 
-import javax.net.ssl.SSLException;
+import java.time.Duration;
 import java.util.function.Consumer;
 
 @Component
@@ -38,88 +35,68 @@ import java.util.function.Consumer;
 public class GenericRestController {
     @PostMapping("/**")
     @CircuitBreaker(name = "post", fallbackMethod = "awsFallbackMethod")
-    public Mono<String> post(RequestEntity<String> req) {
-        Mono<String> user = null;
-        try {
-            HttpHeaders h = req.getHeaders();
-//            log.info("pg-api-gateway hit: {}",new JSONObject(req));
-            Consumer<HttpHeaders> consumer = it -> it.addAll(h);
-            user = WebClient.builder().
-                    clientConnector(new ReactorClientHttpConnector(HttpClient.create().secure(t -> {
-                        try {
-                            t.sslContext(SslContextBuilder
-                                    .forClient()
-                                    .trustManager(InsecureTrustManagerFactory.INSTANCE)
-                                    .build());
-                        } catch (SSLException e) {
-                        }
-                    }))).
-
-
-                    build().post().uri(UriComponentsBuilder.newInstance()
-                            .host("apigw.pg2nonprod.paytm.com")
-                            .path(req.getUrl().getPath())
-                            .scheme("https")
-                            .build().toUri()
-                    )
-                    .headers(consumer)
-                    .headers(headers -> headers.remove(HttpHeaders.HOST))
-                    .header("Host", "apigw.pg2nonprod.paytm.com")
-                    .body(BodyInserters.fromValue(req.getBody())).retrieve()
-                    .bodyToMono(String.class);
-            return user;
-
-        } catch (WebClientResponseException.TooManyRequests e) {
-            log.error("API_GW_Error : {}", e.getStackTrace());
-            return user;
-        }
+    public Mono<ResponseEntity<Void>> post(RequestEntity<String> req) {
+        Mono<ResponseEntity<Void>> user = null;
+        HttpHeaders h = req.getHeaders();
+//            log.info("pg-api-gateway hit: {}", new JSONObject(req));
+        Consumer<HttpHeaders> consumer = it -> it.addAll(h);
+        user = WebClient.builder().
+                build().post().uri(UriComponentsBuilder.newInstance()
+                        .host("apigw.pg2nonprod.paytm.com")
+                        .path(req.getUrl().getPath())
+                        .scheme("https")
+                        .build().toUri()
+                )
+                .headers(consumer)
+                .headers(headers -> headers.remove(HttpHeaders.HOST))
+                .header("Host", "apigw.pg2nonprod.paytm.com")
+                .body(BodyInserters.fromValue(req.getBody()))
+                .retrieve().toBodilessEntity()
+                .onErrorReturn(WebClientResponseException.TooManyRequests.class, ResponseEntity.status(HttpResponseStatus.TOO_MANY_REQUESTS.code()).build())
+                .onErrorReturn(WebClientResponseException.GatewayTimeout.class, ResponseEntity.status(HttpResponseStatus.GATEWAY_TIMEOUT.code()).build())
+                .onErrorReturn(WebClientResponseException.InternalServerError.class, ResponseEntity.status(HttpResponseStatus.INTERNAL_SERVER_ERROR.code()).build())
+                .onErrorReturn(WebClientResponseException.BadGateway.class, ResponseEntity.status(HttpResponseStatus.BAD_GATEWAY.code()).build())
+                .onErrorReturn(WebClientResponseException.BadRequest.class, ResponseEntity.status(HttpResponseStatus.BAD_REQUEST.code()).build())
+                .onErrorReturn(WebClientResponseException.ServiceUnavailable.class, ResponseEntity.status(HttpResponseStatus.SERVICE_UNAVAILABLE.code()).build())
+                .doOnNext(response -> log.info("Response from Custom-API-GW: {}", response))
+                .timeout(Duration.ofMillis(29500));
+        return user;
     }
 
-    public Mono<String> awsFallbackMethod(RequestEntity<String> req, Throwable th) {
+    public Mono<ResponseEntity<Void>> awsFallbackMethod(RequestEntity<String> req, Throwable th) {
 
 
-        Mono<String> user = null;
-        try {
-            if (th instanceof WebClientResponseException && ((WebClientResponseException) th).getStatusCode().is4xxClientError()) {
-                user = Mono.just(HttpResponseStatus.TOO_MANY_REQUESTS.toString());
-                return user;
-            }
+        Mono<ResponseEntity<Void>> user = null;
+//        try {
+//            if (th instanceof WebClientResponseException && ((WebClientResponseException) th).getStatusCode().is4xxClientError()) {
+//                user = Mono.just(HttpResponseStatus.TOO_MANY_REQUESTS.toString());
+//                return user;
+//            }
 
-            HttpHeaders h = req.getHeaders();
-//            h.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-//            h.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-
-            Consumer<HttpHeaders> consumer = it -> it.addAll(h);
-            log.info("amazon-api-gateway hit {}", th.getStackTrace());
-            user = WebClient.builder().
-
-
-                    clientConnector(new ReactorClientHttpConnector(HttpClient.create().secure(t -> {
-                        try {
-                            t.sslContext(SslContextBuilder
-                                    .forClient()
-                                    .trustManager(InsecureTrustManagerFactory.INSTANCE)
-                                    .build());
-                        } catch (SSLException e) {
-                            throw new RuntimeException(e);
-                        }
-                    })))
-
-
-                    .build().post().uri(UriComponentsBuilder.newInstance()
-                            .host("dev-qa.pg2nonprod.paytm.com")
-                            .path(req.getUrl().getPath())
-                            .scheme("https")
-                            .build().toUri()
-                    )
-                    .headers(consumer)
-                    .headers(headers -> headers.remove(HttpHeaders.HOST))
-                    .header("Host", "dev-qa.pg2nonprod.paytm.com")
-                    .body(BodyInserters.fromValue(req.getBody())).retrieve().bodyToMono(String.class);
-            return user;
-        } catch (WebClientResponseException.TooManyRequests e) {
-            log.error("Fallback Error : {}", e.getStackTrace());
-        }
-        return Mono.just(HttpResponseStatus.INTERNAL_SERVER_ERROR.reasonPhrase());
+        HttpHeaders h = req.getHeaders();
+        Consumer<HttpHeaders> consumer = it -> it.addAll(h);
+        log.info("amazon-api-gateway hit {}", th.getStackTrace());
+        user = WebClient.builder()
+                .build().post().uri(UriComponentsBuilder.newInstance()
+                        .host("dev-qa.pg2nonprod.paytm.com")
+                        .path(req.getUrl().getPath())
+                        .scheme("https")
+                        .build().toUri()
+                )
+                .headers(consumer)
+                .headers(headers -> headers.remove(HttpHeaders.HOST))
+                .header("Host", "dev-qa.pg2nonprod.paytm.com")
+                .body(BodyInserters.fromValue(req.getBody()))
+                .retrieve().toBodilessEntity()
+                .onErrorReturn(WebClientResponseException.TooManyRequests.class, ResponseEntity.status(HttpResponseStatus.TOO_MANY_REQUESTS.code()).build())
+                .onErrorReturn(WebClientResponseException.GatewayTimeout.class, ResponseEntity.status(HttpResponseStatus.GATEWAY_TIMEOUT.code()).build())
+                .onErrorReturn(WebClientResponseException.InternalServerError.class, ResponseEntity.status(HttpResponseStatus.INTERNAL_SERVER_ERROR.code()).build())
+                .onErrorReturn(WebClientResponseException.BadGateway.class, ResponseEntity.status(HttpResponseStatus.BAD_GATEWAY.code()).build())
+                .onErrorReturn(WebClientResponseException.BadRequest.class, ResponseEntity.status(HttpResponseStatus.BAD_REQUEST.code()).build())
+                .onErrorReturn(WebClientResponseException.ServiceUnavailable.class, ResponseEntity.status(HttpResponseStatus.SERVICE_UNAVAILABLE.code()).build())
+                .doOnNext(response -> log.info("Response from Amazon-API-GW: {}", response))
+                .timeout(Duration.ofSeconds(30));
+        return user;
     }
 }
+
