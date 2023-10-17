7bb4c633f6a85b11a724e89e91f4f764e1bce279
honey-joshi <honey.joshi@paytm.com>
2023-10-17 14:07:15 +0530

    logics 1.9

--- circuit.breaker/src/main/java/com/cloud/circuit/breaker/controller/GenericRestController.java
+++ circuit.breaker/src/main/java/com/cloud/circuit/breaker/controller/GenericRestController.java
@@ -4,6 +4,7 @@ package com.cloud.circuit.breaker.controller;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -35,8 +36,8 @@ import java.util.function.Consumer;
 public class GenericRestController {
     @PostMapping("/**")
     @CircuitBreaker(name = "post", fallbackMethod = "awsFallbackMethod")
-    public Mono<ResponseEntity<Void>> post(RequestEntity<String> req) {
-        Mono<ResponseEntity<Void>> user = null;
+    public Mono<ResponseEntity<String>> post(RequestEntity<String> req) {
+        Mono<ResponseEntity<String>> user = null;
         HttpHeaders h = req.getHeaders();
 //            log.info("pg-api-gateway hit: {}", new JSONObject(req));
         Consumer<HttpHeaders> consumer = it -> it.addAll(h);
@@ -51,28 +52,20 @@ public class GenericRestController {
                 .headers(headers -> headers.remove(HttpHeaders.HOST))
                 .header("Host", "apigw.pg2nonprod.paytm.com")
                 .body(BodyInserters.fromValue(req.getBody()))
-                .retrieve().toBodilessEntity()
-                .onErrorReturn(WebClientResponseException.TooManyRequests.class, ResponseEntity.status(HttpResponseStatus.TOO_MANY_REQUESTS.code()).build())
+                .retrieve().toEntity(String.class)
+                .onErrorReturn(WebClientResponseException.TooManyRequests.class, ResponseEntity.status(HttpResponseStatus.TOO_MANY_REQUESTS.code()).body("Too Many Request"))
                 .onErrorReturn(WebClientResponseException.GatewayTimeout.class, ResponseEntity.status(HttpResponseStatus.GATEWAY_TIMEOUT.code()).build())
                 .onErrorReturn(WebClientResponseException.InternalServerError.class, ResponseEntity.status(HttpResponseStatus.INTERNAL_SERVER_ERROR.code()).build())
                 .onErrorReturn(WebClientResponseException.BadGateway.class, ResponseEntity.status(HttpResponseStatus.BAD_GATEWAY.code()).build())
                 .onErrorReturn(WebClientResponseException.BadRequest.class, ResponseEntity.status(HttpResponseStatus.BAD_REQUEST.code()).build())
                 .onErrorReturn(WebClientResponseException.ServiceUnavailable.class, ResponseEntity.status(HttpResponseStatus.SERVICE_UNAVAILABLE.code()).build())
-                .doOnNext(response -> log.info("Response from Custom-API-GW: {}", response))
+                .doOnNext(response -> log.info("Response from Custom-API-GW: {}", new JSONObject(response)))
                 .timeout(Duration.ofMillis(29500));
         return user;
     }
 
-    public Mono<ResponseEntity<Void>> awsFallbackMethod(RequestEntity<String> req, Throwable th) {
-
-
-        Mono<ResponseEntity<Void>> user = null;
-//        try {
-//            if (th instanceof WebClientResponseException && ((WebClientResponseException) th).getStatusCode().is4xxClientError()) {
-//                user = Mono.just(HttpResponseStatus.TOO_MANY_REQUESTS.toString());
-//                return user;
-//            }
-
+    public Mono<ResponseEntity<String>> awsFallbackMethod(RequestEntity<String> req, Throwable th) {
+        Mono<ResponseEntity<String>> user = null;
         HttpHeaders h = req.getHeaders();
         Consumer<HttpHeaders> consumer = it -> it.addAll(h);
         log.info("amazon-api-gateway hit {}", th.getStackTrace());
@@ -87,7 +80,7 @@ public class GenericRestController {
                 .headers(headers -> headers.remove(HttpHeaders.HOST))
                 .header("Host", "dev-qa.pg2nonprod.paytm.com")
                 .body(BodyInserters.fromValue(req.getBody()))
-                .retrieve().toBodilessEntity()
+                .retrieve().toEntity(String.class)
                 .onErrorReturn(WebClientResponseException.TooManyRequests.class, ResponseEntity.status(HttpResponseStatus.TOO_MANY_REQUESTS.code()).build())
                 .onErrorReturn(WebClientResponseException.GatewayTimeout.class, ResponseEntity.status(HttpResponseStatus.GATEWAY_TIMEOUT.code()).build())
                 .onErrorReturn(WebClientResponseException.InternalServerError.class, ResponseEntity.status(HttpResponseStatus.INTERNAL_SERVER_ERROR.code()).build())
@@ -95,7 +88,7 @@ public class GenericRestController {
                 .onErrorReturn(WebClientResponseException.BadRequest.class, ResponseEntity.status(HttpResponseStatus.BAD_REQUEST.code()).build())
                 .onErrorReturn(WebClientResponseException.ServiceUnavailable.class, ResponseEntity.status(HttpResponseStatus.SERVICE_UNAVAILABLE.code()).build())
                 .doOnNext(response -> log.info("Response from Amazon-API-GW: {}", response))
-                .timeout(Duration.ofSeconds(30));
+                .timeout(Duration.ofSeconds(35));
         return user;
     }
 }
